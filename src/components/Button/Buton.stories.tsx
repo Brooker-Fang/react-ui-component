6e66ad49c0ebfe44c0960b7262df74ad437dc24e
KingComedy <1455445725@qq.com>
2022-02-11 19:48:08 +0800

    add storybook

---
+++ src/components/Button/Buton.stories.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import '../../styles/index.scss'
+import Button, {  } from './button';
+// default
+const defaultButton = () => (<Button >default</Button>)
+
+// 不同尺寸
+const buttonWithSize = () => (
+  <>
+    <Button size='lg'>large button</Button>
+    <Button size='sm'>small button</Button>
+  </>
+)
+storiesOf('Button Component', module)
+  .add('默认 Button', defaultButton)
+  .add('不同尺寸 button', buttonWithSize)
+
+
